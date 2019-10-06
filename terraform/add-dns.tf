d2c5807ffda02df93d28a907419e4e08d5fa8eb6
Colin Simmons <colin.simmons@engineerbetter.com>
2019-10-06 01:34:13 +0100

    one of these days I will read before committing
    this was not that day
    
    Signed-off-by: Colin Simmons <colin.simmons@engineerbetter.com>

--- terraform/add-dns.tf
+++ terraform/add-dns.tf
@@ -7,8 +7,8 @@ data "azurerm_dns_zone" "parent_zone" {
 
 resource "azurerm_dns_ns_record" "pks_ns_record" {
   name                = "${var.env_name}"
-  zone_name           = "${azurerm_dns_zone.parent_zone.name}"
-  resource_group_name = "${data.azurerm_dns_zone.parent_zone.name}"
+  zone_name           = "${data.azurerm_dns_zone.parent_zone.name}"
+  resource_group_name = "${var.parent_dns_rg}"
   ttl                 = 300
 
   records = "${module.infra.dns_zone_name_servers}"
