6eab5a2db828177ec3ab8992e8413722a7c05fae
Colin Simmons <colin.simmons@engineerbetter.com>
2019-10-07 20:32:16 +0100

    add dns record for api
    Signed-off-by: Colin Simmons <colin.simmons@engineerbetter.com>

--- terraform/modules/pks/extra-outputs.tf
+++ terraform/modules/pks/extra-outputs.tf
@@ -1,3 +1,7 @@
 output "pks_lb_name" {
   value = "${var.env_id}-pks-lb"
 }
+
+output "pks_lb_ips" {
+  value = "${azurerm_lb.pks-lb.private_ip_addresses}"
+}
--- terraform/terraforming-pks/dns.tf
+++ terraform/terraforming-pks/dns.tf
@@ -13,3 +13,11 @@ resource "azurerm_dns_ns_record" "pks_ns_record" {
 
   records = ["${module.infra.dns_zone_name_servers}"]
 }
+
+resource "azurerm_dns_a_record" "pks_api" {
+  name                = "api"
+  zone_name           = "${module.infra.dns_zone_name}"
+  resource_group_name = "${module.infra.resource_group_name}"
+  ttl                 = 300
+  records             = ["${module.pks.pks_lb_ips}"]
+}
